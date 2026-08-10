56013cdeaea4fe2046b72c38e2665deb3627e4ee
DILANSH JAIN <dilanshsjain2005@gmail.com>
2026-08-11 01:10:54 +0530

    Time: 1 ms (29.85%), Space: 47.2 MB (82.93%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    int missingNumber(int arr[]) {
+        
+        int n = arr.length;
+        int i = 0;
+        while(i<arr.length){
+            if(arr[i] == i || arr[i] == n) i++;
+            else{
+                int idx = arr[i];
+                swap(arr , i , idx);
+            }
+        }
+        for(i = 0 ; i < n ; i++){
+            if(arr[i] != i) return i;
+        }
+        return n;
+    }
+    
+    private void swap(int[] arr , int i , int idx){
+        int temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
+    }
+    
+    
+}
